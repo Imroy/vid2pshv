b9a73a9eb09eb3dedec4b4252c8a62600aeffb9d
Ian Tester <imroykun@gmail.com>
2017-09-29 17:18:46 +1000

    Maintain video aspect ratio while scaling to fit within 960x544
    Taken from:
    https://trac.ffmpeg.org/wiki/Scaling%20(resizing)%20with%20ffmpeg

--- vid2pshv.sh
+++ vid2pshv.sh
@@ -12,7 +12,7 @@ trap on_exit EXIT INT KILL
 
 echo Starting BSF stream extraction through ffmpeg, please wait...
 ffmpeg -hide_banner -i "$1"\
- -r 30000/1001 -vf scale=960:544 -b:v "${VB:-2}M" -maxrate "${VB:-2}M" -bufsize 2M\
+ -r 30000/1001 -vf scale=960:544:force_original_aspect_ratio=decrease -b:v "${VB:-2}M" -maxrate "${VB:-2}M" -bufsize 2M\
  -c:v libx264 -x264opts "aud=1" -crf 18 -profile:v baseline -preset:v veryslow -g 15 -pix_fmt yuv420p\
  /tmp/baseline-$$.264 || exit
 
