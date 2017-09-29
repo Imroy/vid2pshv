c0dc666fae02872192ae34213f3b0d22e8875031
Ian Tester <imroykun@gmail.com>
2017-09-29 16:33:38 +1000

    Convert the DOS batch file to a UNIX-y shell script
    It's no longer interactive, but who interacts with a shell script? The first argument is the input file, all other options are environment variables, with defaults.
    Output filename is constructed using 'basename', instead of the fixed output.pshv.

---
+++ vid2pshv.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+on_exit() {
+  echo Deleting temp files...
+  rm -fv /tmp/audiotrack-$$.ogg /tmp/baseline-$$.264
+}
+
+echo vid2pshv - PSHV Encoder
+echo -----------------------
+
+trap on_exit EXIT INT KILL
+
+echo Starting BSF stream extraction through ffmpeg, please wait...
+ffmpeg -hide_banner -i "$1"\
+ -r 30000/1001 -vf scale=960:544 -b:v "${VB:-2}M" -maxrate "${VB:-2}M" -bufsize 2M\
+ -c:v libx264 -x264opts "aud=1" -crf 18 -profile:v baseline -preset:v veryslow -g 15 -pix_fmt yuv420p\
+ /tmp/baseline-$$.264 || exit
+
+echo Starting audio extraction through ffmpeg, please wait...
+ffmpeg -hide_banner -i "$1"\
+ -acodec: libvorbis -ac "${AC:-2}" -ar "${AR:-48000}" -vn\
+ /tmp/audiotrack-$$.ogg || exit
+
+echo Starting video encoding, please wait...
+pshv_encoder 29.970 /tmp/audiotrack-$$.ogg /tmp/baseline-$$.264 `basename "$1"`.pshv || exit
+
+echo Video converted successfully, you\'ll find an `basename "$1"`.pshv file.
